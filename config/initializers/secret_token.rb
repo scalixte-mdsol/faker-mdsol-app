37fcb1077be103704f56d112fc9ffc337cf75635
Stanley Calixte <scalixte@mdsol.com>
2014-11-08 18:20:47 -0500

    first commit to develop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FakerMdsolApp::Application.config.secret_key_base = '32b5f3a1d6f0bda0248f0c17ec4c5d5c8a24265e9e2429ba2b32d9e06a083c513ac5ea4f482579e7fc87c4c116e06c7f16f598727222ac250d7e31e9f9a1f273'
